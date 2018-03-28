314b0a7b1dd97b0a8e253640f2ec6205206627d0
t-shidli <t-shidli@microsoft.com>
2018-03-28 15:09:45 +0800

    test Case

--- test.py
+++ test.py
@@ -4,7 +4,6 @@
 from AdaSegment import AdaSegment
 
 if __name__ == '__main__':
-    res = ''
     testCase = [
         "王总和小丽结婚了",
         "柯文哲纠正司仪“恭请市长”说法：别用封建语言",
@@ -13,18 +12,14 @@ if __name__ == '__main__':
         "张浩和胡健康复员回家了",
         "编剧邵钧林和稽道青说",
         "这里有关天培的有关事迹",
-        "龚学平等领导,邓颖超生前"
+        "龚学平等领导,邓颖超生前",
         "微软的比尔盖茨、Facebook的扎克伯格跟桑德博格"
     ]
     segment = AdaSegment.newSegment().enableNameRecognize(True)
+    print "==================="
     for sentence in testCase:
         termList = segment.seg(sentence)
-        m = "["
+        item = []
         for i in termList:
-            a = i.word + "/" + i.nature
-            m += a + ', '
-        m = m.strip(', ')
-        m += ']'
-        print m
-        res += m + '\n'
-    print res
+            item.append(i.word + "/" + i.nature)
+        print ",".join(item)
